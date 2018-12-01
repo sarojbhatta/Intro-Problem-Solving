f931e686133636500e42a8dbcbd485a171f82f2b
Saroj Bhatta <sarojbhatta054.sb@gmail.com>
2018-12-01 03:43:21 -0500

    maps

--- mapLongestValueSet.java
+++ mapLongestValueSet.java
@@ -1,6 +1,7 @@
 /*
-Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths, then set "c" to have the longer value. If the values exist and have the same length, change them both to the empty string in the map.
-
+Modify and return the given map as follows: if the keys "a" and "b" have values that 
+have different lengths, then set "c" to have the longer value. If the values exist 
+and have the same length, change them both to the empty string in the map.
 
 mapAB4({"a": "aaa", "b": "bb", "c": "cake"}) ? {"a": "aaa", "b": "bb", "c": "aaa"}
 mapAB4({"a": "aa", "b": "bbb", "c": "cake"}) ? {"a": "aa", "b": "bbb", "c": "bbb"}
